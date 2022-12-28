63751bcc88a46740e3b5a46d41176ff238840b55
vijay kanaujia <vijaykanaujia3@gmail.com>
2022-12-28 17:41:53 +0530

    initialize project

---
+++ bootstrap/app.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vijay Kanaujia
+ * Date: 28-12-2022
+ * Time: 05:13 PM
+ */
---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vijay Kanaujia
+ * Date: 28-12-2022
+ * Time: 05:09 PM
+ */
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+
+require_once __DIR__.'/../bootstrap/app.php';
